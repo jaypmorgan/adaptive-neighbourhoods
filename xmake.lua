af585425ec1a994b81e9456b1be40227c03a02b9
Jay Paul Morgan <jaymiles17@gmail.com>
2024-04-05 14:18:32 +0100

    Move files

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,8 @@ add_requires("pybind11")
 
 target("main")
     set_kind("binary")
-    add_headerfiles("adaptive_neighbourhoods/src/*.h")
-    add_files("adaptive_neighbourhoods/src/*.cpp")
+    add_headerfiles("src/*.h")
+    add_files("src/*.cpp")
     set_languages("c++11")
     add_packages("pybind11")
 
